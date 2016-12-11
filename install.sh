aae8690178d0a473a6cce7dfa08193e58fd29da4
Brian Newton <brian.newton@lyncstream.com>
2016-12-11 13:51:58 -0600

    Initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install software-properties-common # needed for adding keys?
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+sudo add-apt-repository ppa:mythbuntu/0.28
+sudo add-apt-repository ppa:heyarje/makemkv-beta
+
+sudo apt-get update 
+sudo apt-get install google-chrome-stable mythtv kodi kodi-pvr-mythtv
